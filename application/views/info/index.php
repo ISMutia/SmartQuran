9141f4f066be44cddd283363e10244186bfe6fe8
zalviandyr <zukronalviandy@gmail.com>
2021-09-09 20:24:30 +0700

    add info page

---
+++ application/views/info/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<!-- upto 2 directory depth-->
+<html lang="en-US">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin" />
+	<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700;800&amp;display=swap" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700;800&amp;display=swap" media="print" onload="this.media='all'" />
+	<noscript>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700;800&amp;display=swap" />
+	</noscript>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+	<link href="<?= base_url('assets/info/css/bootstrap-icons/bootstrap-icons.css?ver=1.2.0') ?>" rel="stylesheet">
+	<link href="<?= base_url('assets/css/aos.css?ver=1.2.0') ?>" rel="stylesheet">
+	<link href="<?= base_url('assets/css/info.css?ver=1.2.0') ?>" rel="stylesheet">
+	<noscript>
+		<style type="text/css">
+			[data-aos] {
+				opacity: 1 !important;
+				transform: translate(0) scale(1) !important;
+			}
+		</style>
+	</noscript>
+</head>
+
+<body id="top">
+	<header class="bg-light">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light" id="header-nav" role="navigation">
+			<div class="container" style="text-align: center"><a class="link-dark navbar-brand site-title mb-0" href="#">
+					<marquee>Girl's PNP II</marquee>
+			</div>
+		</nav>
+	</header>
+	<div class="page-content">
+		<div id="content">
+			<div class="section px-2 px-lg-4 pt-5" id="profile">
+				<div class="container">
+					<div class="text-center mb-5">
+					</div>
+					<div class="grid bp-gallery pb-3" data-aos="zoom-in-up" data-aos-delay="100">
+						<div class="grid-sizer"></div>
+						<div class="grid-item"><a href="https://www.instagram.com/adikmutia/" target="_blank">
+								<figure class="portfolio-item"><img src="<?= base_url('assets/img/person/mutia.jpg') ?>" data-bp="<?= base_url('assets/img/person/mutia.jpg') ?>" />
+									<figcaption>
+										<h4 class="h5 mb-0">I.S. Mutia</h4>
+										<div>Jadilah pribadi yang mandiri yang tidak terlalu bergantung pada siapapun karena bayangan mu pun akan meninggalkan mu dan jadilah pribadi yang selalu berusaha sebelum meminta tolong.</div>
+									</figcaption>
+								</figure>
+							</a></div>
+						<div class="grid-item"><a href="https://www.instagram.com/callme.rzlyana/" target="_blank">
+								<figure class="portfolio-item"><img src="<?= base_url('assets/img/person/lin.jpeg') ?>" data-bp="<?= base_url('assets/img/person/lin.jpeg') ?>" data-caption="Example of an optional caption." />
+									<figcaption>
+										<h4 class="h5 mb-0">Rozliyana Binti Latola</h4>
+										<div>Setiap perjuangan butuh pengorbanan, dan
+											semoga apa yang diperjuangkan membuahkan hasil yang penuh dengan keindahan.</div>
+									</figcaption>
+								</figure>
+							</a></div>
+						<div class="grid-item"><a href="https://www.instagram.com/afzaso/" target="_blank">
+								<figure class="portfolio-item"><img src="<?= base_url('assets/img/person/fina.jpeg') ?>" data-bp="<?= base_url('assets/img/person/fina.jpeg') ?>" />
+									<figcaption>
+										<h4 class="h5 mb-0">Afza Sorfina</h4>
+										<div>Dunia ini penuh dengan orang-orang baik. Jika kamu tidak dapat menemukannya, maka jadilah salah satunya.</div>
+									</figcaption>
+								</figure>
+							</a></div>
+						<div class="grid-item"><a href="https://www.instagram.com/_nsp14/" target="_blank">
+								<figure class="portfolio-item"><img src="<?= base_url('assets/img/person/putri.jpeg') ?>" data-bp="<?= base_url('assets/img/person/putri.jpeg') ?>" data-caption="Example of an optional caption." />
+									<figcaption>
+										<h4 class="h5 mb-0">Nuria Sisma Putri</h4>
+										<div>Walaupun terasa tak seberapa, syukurilah apa pun yang kamu punya. Karena nikmat setiap orang pasti berbeda.</div>
+									</figcaption>
+								</figure>
+							</a></div>
+					</div>
+				</div>
+			</div>
+			<footer class="pt-4 pb-4 text-center bg-light">
+				<div class="container">
+					<div class="my-3">
+						<div class="h4">Mutia & Lin & Fina & Putri</div>
+						<p>Thank You :)</p>
+					</div>
+				</div>
+		</div>
+	</div>
+	<div id="scrolltop"><a class="btn btn-secondary" href="#top"><span class="icon"><i class="fas fa-angle-up fa-x"></i></span></a></div>
+	<script src="<?= base_url('assets/js/imagesloaded.pkgd.min.js?ver=1.2.0') ?>"></script>
+	<script src="<?= base_url('assets/js/masonry.pkgd.min.js?ver=1.2.0') ?>"></script>
+	<script src="<?= base_url('assets/js/aos.min.js?ver=1.2.0') ?>"></script>
+	<script src="<?= base_url('assets/js/info.js?ver=1.2.0') ?>"></script>
+</body>
+
+</html>
